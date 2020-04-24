71dd8a79caa87a29f15a78788420915803e456fb
oulu_hillbilly <svaranka@gmail.com>
2020-04-24 16:52:58 +0300

    aa

--- app/src/main/java/com/example/rouman/SettingsActivity.kt
+++ app/src/main/java/com/example/rouman/SettingsActivity.kt
@@ -9,11 +9,11 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.settings, SettingsFragment())
-            .commit()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(R.id.settings, SettingsFragment())
+//            .commit()
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
